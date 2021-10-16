3240763f0fce3370059d4ec1204f7ba2e6057375
Daniel Rudolf <github.com@daniel-rudolf.de>
2021-10-16 17:01:34 +0200

    Initial code commit
    Add 'setup.sh'

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ci-tools -- setup.sh
+# Downloads and setup tools from @SGSGermany's `ci-tools` collection.
+#
+# Copyright (c) 2021  SGS Serious Gaming & Simulations GmbH
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see LICENSE file or <https://opensource.org/licenses/MIT>.
+#
+# SPDX-License-Identifier: MIT
+# License-Filename: LICENSE
+
+set -e
+export LC_ALL=C
+
+GITHUB_SERVER_URL="https://github.com"
+REPOSITORY="SGSGermany/ci-tools"
+
+GIT_DIR="$1"
+if [ -z "$GIT_DIR" ]; then
+    GIT_DIR="$(mktemp -d)"
+fi
+
+echo "Cloning 'ci-tools' repository from '$GITHUB_SERVER_URL/$REPOSITORY.git'..." >&2
+git clone --depth=1 "$GITHUB_SERVER_URL/$REPOSITORY.git" "$GIT_DIR" >&2
+
+COMMIT_SHA="$(git -C "$GIT_DIR" show -s --format=%h HEAD)"
+COMMIT_DATE="$(git -C "$GIT_DIR" show -s --format=%ci HEAD)"
+echo "Using CI tools as of commit $COMMIT_SHA from $COMMIT_DATE" >&2
+
+echo "Setting 'CI_TOOLS_PATH' environment variable: $GIT_DIR/src" >&2
+printf 'export %s="%s"' "CI_TOOLS_PATH" "$GIT_DIR/src"
