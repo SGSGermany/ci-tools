792ce800405a92730cd906142a3139c7abe4db70
Daniel Rudolf <github.com@daniel-rudolf.de>
2022-06-20 22:42:47 +0200

    generate-image-tags.sh: Update version build suffix to include CI info
    Also upgrades the script to use the latest CI tools.

--- src/containers/generate-image-tags.sh
+++ src/containers/generate-image-tags.sh
@@ -13,12 +13,19 @@
 set -eu -o pipefail
 export LC_ALL=C
 
-git_ls_tags() {
-    git ls-remote "origin" "refs/tags/$1" | cut -f 2 | sort_semver
-}
+[ -v CI_TOOLS ] && [ "$CI_TOOLS" == "SGSGermany" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS' not set or invalid" >&2; exit 1; }
 
-sort_semver() {
-    sed '/-/!{s/$/_/}' | sort -V -r | sed 's/_$//'
+[ -v CI_TOOLS_PATH ] && [ -d "$CI_TOOLS_PATH" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS_PATH' not set or invalid" >&2; exit 1; }
+
+source "$CI_TOOLS_PATH/helper/common.sh.inc"
+
+git rev-parse --git-dir > /dev/null 2>&1 \
+    || { echo "Invalid build environment: This is no Git repository" >&2; exit 1; }
+
+git_ls_tags() {
+    git ls-remote "origin" "refs/tags/$1" | cut -f2 | sort_semver
 }
 
 GIT_REF="${1:-}"
@@ -26,33 +33,34 @@ if [ -z "$GIT_REF" ]; then
     echo "Missing required parameter 'GIT_REF'" >&2
     exit 1
 fi
-if ! git rev-parse --git-dir > /dev/null 2>&1; then
-    echo "Invalid build environment: This is no Git repository" >&2
-    exit 1
+
+BUILD_INFO=""
+if [[ "${2:-}" =~ ^[a-zA-Z0-9_.-]+$ ]]; then
+    BUILD_INFO=".${2,,}"
 fi
 
-IMAGE_TAG_DATE="$(date -u +'%Y%m%d%H%M')"
+BUILD_INFO="$(date --utc +'%Y%m%d')$BUILD_INFO"
 
 IMAGE_TAGS=()
 if [[ "$GIT_REF" =~ ^refs/tags/ ]]; then
     CURRENT_TAG="${GIT_REF:10}"
 
     IMAGE_TAG="$(echo "$CURRENT_TAG" | sed -e 's/\//-/g')"
-    IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+    IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
 
     if [[ "$CURRENT_TAG" =~ ^v([0-9]+)(\.([0-9]+))?(\.([0-9]+))?(-[a-zA-Z0-9.-]+)?(\+[a-zA-Z0-9.-]+)?$ ]]; then
         if [ -n "${BASH_REMATCH[7]}" ]; then
             IMAGE_TAG="v${BASH_REMATCH[1]}${BASH_REMATCH[2]}${BASH_REMATCH[4]}${BASH_REMATCH[6]}"
-            IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+            IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
         fi
         if [ -z "${BASH_REMATCH[6]}" ]; then
             if [ -n "${BASH_REMATCH[5]}" ]; then
                 IMAGE_TAG="v${BASH_REMATCH[1]}.${BASH_REMATCH[3]}"
-                IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+                IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
             fi
             if [ -n "${BASH_REMATCH[3]}" ]; then
                 IMAGE_TAG="v${BASH_REMATCH[1]}"
-                IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+                IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
             fi
         fi
     fi
@@ -65,19 +73,19 @@ elif [[ "$GIT_REF" =~ ^refs/heads/ ]]; then
     CURRENT_BRANCH="${GIT_REF:11}"
 
     IMAGE_TAG="dev-$(echo "$CURRENT_BRANCH" | sed 's/[^a-zA-Z0-9._-]\{1,\}/-/g')"
-    IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+    IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
 elif [[ "$GIT_REF" =~ ^refs/pull/([0-9]+)/merge$ ]]; then
     CURRENT_PR="${BASH_REMATCH[1]}"
 
     IMAGE_TAG="pr-$CURRENT_PR"
-    IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+    IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
 elif [ "$(git rev-parse --verify "$GIT_REF" 2> /dev/null)" != "$GIT_REF" ]; then
     echo "Invalid Git reference given: $GIT_REF" >&2
     exit 1
 fi
 
 IMAGE_TAG="sha-$(git rev-parse "$GIT_REF")"
-IMAGE_TAGS+=( "$IMAGE_TAG" "${IMAGE_TAG}_$IMAGE_TAG_DATE" )
+IMAGE_TAGS+=( "$IMAGE_TAG" "$IMAGE_TAG-$BUILD_INFO" )
 
 # the first tag always represents the generic name of this particular Git reference
 # (i.e. the generic name of this tag, branch, or PR, no matter which commit points to it right now)
